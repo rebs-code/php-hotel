e39ae4c8c1daa0eeb1458aebdc5a60f2f842e79d
rebs-code <jack.rebussi@gmail.com>
2024-01-09 16:30:30 +0100

    feat: foreach loop prints all data

--- index.php
+++ index.php
@@ -50,6 +50,11 @@
             'distance_to_center' => 50
         ],
     ];
+    foreach ($hotels as $hotel_info) {
+        foreach ($hotel_info as $hotel) {
+            echo $hotel;
+        }
+    }
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
